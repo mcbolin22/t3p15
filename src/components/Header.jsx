08e87232570e760840bb3516283ee05a4256a0c0
Colin <mcbolin@hotmail.com>
2024-07-04 13:12:41 +1000

    feat: header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import { NavLink } from "react-router-dom";
+import "../styles/header.css"
 
 
 export default function Header(){
 
-    return(<>
-        <div id="headerBranding">
-            CSS Generator
-        </div>
-        <nav id="headerNavbar">
-            <NavLink to={"/"}>Home</NavLink>
-            <NavLink to={"/generator"}>Generator</NavLink>
-            {/* <Navlink to={"/generator/saved"}>Saved Themes</Navlink> */}
-        </nav>
-    </>)
+    return(
+        <header>
+            <div id="headerBranding">
+                CSS Generator
+            </div>
+            <nav id="headerNavbar">
+                <NavLink to={"/"}>Home</NavLink>
+                <NavLink to={"/generator"}>Generator</NavLink>
+            </nav>
+        </header>
+    )
 }
